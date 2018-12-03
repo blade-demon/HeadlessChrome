7e49db1416e74f5300c9c0d3ba4eb37ab66e6f4a
blade-demon <xuziwei89@gmail.com>
2018-12-03 19:28:57 +0800

    完成抖音验证码的验证功能

---
+++ app.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return a + b;
+}
+
+debugger;
+console.log("debugger start");
+debugger;
+add(3,5);
+debugger;
--- scrapy-douyin-data.js
+++ scrapy-douyin-data.js
@@ -1,5 +1,8 @@
 const puppeteer = require("puppeteer");
 const axios = require("axios");
+const request = require("request-promise");
+const FormData = require("form-data");
+const fs = require("fs");
 
 const USERNAME_SELECTOR = "input#account";
 const PASSWORD_SELECTOR = "input[name='password']";
@@ -9,98 +12,130 @@ const LOGINBTN_SELECTOR = "input[name='submitBtn']";
 
 const ERRORMESSAGE_SELECTOR = ".error-msg";
 
-// 数据概览按钮
-const DETAIL_BUTTON_SELECTOR = "ul.main-nav li:nth-child(1)";
-const MORE_BUTTON_SELECTOR = ".card div.ft.unexpand";
-// 粉丝分析
-const FANS_BUTTON_SELECTOR = "ul.main-nav li:nth-child(2)";
-// 博文分析
-const BLOGDETAIL_BUTTON_SELECTOR = "ul.main-nav li:nth-child(3)";
-// 互动分析
-const INTERACT_BUTTON_SELECTOR = "ul.main-nav li:nth-child(4)";
-// 文章分析
-const ARTICLE_BUTTON_SELECTOR = "ul.main-nav li:nth-child(6)";
+// 数据分析
+const DATA_ANALYTICS_URI = `https://mp.toutiao.com/profile_v3/douyin/data-analysis`;
 
-const { douyin, dama } = require("./creds");
-let imageData = "";
+//
 
-// (async () => {
-//   try {
-//     const browser = await puppeteer.launch({ headless: false });
-//     const uri =
-//       "https://sso.toutiao.com/login/?service=https://mp.toutiao.com/sso_confirm/?redirect_url=/";
-//     const page = await browser.newPage();
-//     page.setDefaultNavigationTimeout(60 * 1000);
-//     await page.setViewport({ width: 1600, height: 900 });
-//     await page.goto(uri);
+const { douyin, captcha } = require("./creds");
 
-//     await page.waitFor(USERNAME_SELECTOR);
-//     await page.waitFor(PASSWORD_SELECTOR);
-//     await page.waitFor(LOGINBTN_SELECTOR);
-//     // 获取验证码码图片
-//     //imageData = await page.$eval(CAPTCHAIMG_SELECTOR, i => i.src);
-//     const captchaImage = await page.waitForSelector(CAPTCHAIMG_SELECTOR);
-//     imageData = await captchaImage.screenshot({
-//       path: "./tmp/douyin/captcha.png",
-//       omitBackground: false
-//     });
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const uri =
+      "https://sso.toutiao.com/login/?service=https://mp.toutiao.com/sso_confirm/?redirect_url=/";
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(60 * 1000);
+    await page.setViewport({ width: 1600, height: 900 });
+    await page.goto(uri);
 
-//     await page.waitFor(3 * 1000);
-//     await page.type(USERNAME_SELECTOR, douyin.username);
-//     await page.type(PASSWORD_SELECTOR, douyin.password);
-//     await page.type(CAPTCHAINPUT_SELECTOR, getCode());
-//     await page.click(LOGINBTN_SELECTOR);
-//     // await page.waitFor(3 * 1000);
-//     // if (page.$(ERRORMESSAGE_SELECTOR)) {
-//     //   console.log("验证码错误！");
-//     // } else {
-//     //   console.log("验证码成功！");
-//     // }
+    await page.waitFor(USERNAME_SELECTOR);
+    await page.waitFor(PASSWORD_SELECTOR);
+    await page.waitFor(LOGINBTN_SELECTOR);
 
-//     // process.exit(0);
-//   } catch (error) {
-//     console.log(error);
-//     // await browser.close();
-//     // process.exit(-1);
-//   }
-// })();
+    await page.type(USERNAME_SELECTOR, douyin.username);
+    await page.type(PASSWORD_SELECTOR, douyin.password);
 
-getCode();
+    // 获得验证码图片
+    await getScreenshot(page);
 
-// 获得打码结果
-async function getCode() {
-  const result = await uploadImg({ ...dama });
-  console.log(result.data);
-  return "2345";
+    let code = "";
+    while (!code) {
+      let result = await startGetCode();
+      if (result.includes("OK")) {
+        code = result;
+        await page.type(CAPTCHAINPUT_SELECTOR, code.slice(3));
+        await page.click(LOGINBTN_SELECTOR);
+        await page.waitFor(3 * 1000);
+
+        console.log(page.url());
+        // 返回的验证码不匹配
+        if (page.url() === uri) {
+          code = "";
+          // 清空输入框
+          // await page.click(CAPTCHAINPUT_SELECTOR, { clickCount: 3 });
+          // await page.keyboard.press("Backspace");
+          // 重新获得验证码图片
+          await getScreenshot(page);
+        } else {
+          console.log("验证码获取并且验证成功！");
+        }
+      } else {
+        await page.click(".captcha-wrap");
+        console.log("获得的验证码失败，刷新新的验证码");
+      }
+    }
+
+    // process.exit(0);
+  } catch (error) {
+    console.log(error);
+    // await browser.close();
+    // process.exit(-1);
+  }
+})();
+
+async function startGetCode() {
+  return new Promise(async resolve => {
+    try {
+      const resultId = await uploadImg(captcha.apikey);
+      console.log("获得了resultId:", resultId.slice(3));
+      const interval = await setInterval(async () => {
+        // 每隔10s调用一次
+        const codeResult = await getResult(resultId.slice(3));
+        if (codeResult.includes("OK")) {
+          // 验证码获取成功
+          console.log("验证码获取成功！" + codeResult);
+          clearInterval(interval);
+          resolve(codeResult);
+        } else if (codeResult === "CAPCHA_NOT_READY") {
+          // 正在识别
+          console.log("正在等待识别验证码结果!10秒后重试！");
+        } else if (codeResult === "ERROR_CAPTCHA_UNSOLVABLE") {
+          // 验证码无法识别
+          clearInterval(interval);
+          resolve("ERROR_CAPTCHA_UNSOLVABLE");
+        } else {
+          // 其他错误
+          clearInterval(interval);
+          console.log("出现错误", codeResult);
+          resolve(codeResult);
+        }
+      }, 10000);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 
 /**
- * 云打码文档：http://www.yundama.com/apidoc/
+ * 2captcha文档： https://2captcha.com/2captcha-api#solving_normal_captcha
  */
 
-// 返回数据：{"ret":0,"cid":1000000}，ret=0则上传成功
-async function uploadImg({
-  username,
-  password,
-  appid,
-  appkey,
-  codetype,
-  timeout
-}) {
-  return axios.post("http://api.yundama.com/api.php?method=upload", {
-    username,
-    password,
-    appid,
-    appkey,
-    codetype,
-    timeout,
-    enctype: "multipart/form-data",
-    data: "/Users/xuziwei/Desktop/项目/headless-chrome/tmp/douyin/captcha.png"
-  });
+async function uploadImg(apikey) {
+  console.log(apikey);
+  let formData = {
+    key: apikey,
+    file: fs.createReadStream(__dirname + "\\tmp\\douyin\\captcha.png")
+  };
+  return request
+    .post({ url: "http://2captcha.com/in.php", formData: formData })
+    .then(body => {
+      return body;
+    })
+    .catch(err => {
+      throw err;
+    });
 }
+
 // 获取结果接口（循环调用此接口直到有识别结果）
-async function getResult(cid) {
-  return axios.get("http://api.yundama.com/api.php?method=result", { cid });
+async function getResult(resultId) {
+  return axios
+    .get(
+      `http://2captcha.com/res.php?id=${resultId}&action=get&key=${
+        captcha.apikey
+      }`
+    )
+    .then(res => res.data);
 }
 
 // 查询余额接口
@@ -121,3 +156,13 @@ const getReport = ({ username, password, appid, appkey }) =>
     appid,
     appkey
   });
+
+async function getScreenshot(page) {
+  // 获取验证码码图片
+  //imageData = await page.$eval(CAPTCHAIMG_SELECTOR, i => i.src);
+  const captchaImage = await page.waitForSelector(CAPTCHAIMG_SELECTOR);
+  imageData = await captchaImage.screenshot({
+    path: "./tmp/douyin/captcha.png",
+    omitBackground: false
+  });
+}
